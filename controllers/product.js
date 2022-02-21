a42d0817ad8729aae594c754b9573f77c411fb08
sabin-nepal <sabinnepal2k17@gmail.com>
2022-02-21 13:49:55 +0545

    product update delete and fetch

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require('../models/Product')
 
-exports.addProduct = async(req,res) => {
-    const {title,inProduct,outProduct,image} = req.body;
+exports.addProduct = async (req, res) => {
+    const { title, inProduct, outProduct, image } = req.body;
     const user_id = req.user.id;
     const product = new Product({
         title,
@@ -10,11 +10,77 @@ exports.addProduct = async(req,res) => {
         image,
         user_id,
     })
-    try{
+    try {
         var response = await product.save()
         res.status(201).json(response);
     }
-    catch(error){
-        res.status(500).jso(error);
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.fetchProducts = async (req, res) => {
+    try {
+        const product = await Product.find();
+        if (!product) {
+            return res.status(404).json({
+                error: 'No Product Found'
+            })
+        }
+        res.status(200).json(product);
+    }
+    catch (err) {
+        res.status(404).json(err);
+    }
+}
+
+exports.getProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({
+                error: 'No Product Found'
+            })
+        }
+        res.status(200).json(product)
+    }
+    catch (err) {
+        res.status(404).json(err);
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { title, inproduct, outproduct, imageUrl } = req.body
+    try {
+        const response = await Product.findByIdAndUpdate(id, {
+            title: title,
+            inProduct: inproduct,
+            outProduct: outproduct,
+            image: imageUrl
+
+        }, { new: true });
+        res.status(201).json(response);
+    }
+    catch (err) {
+        res.status(404).json(err);
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        var response = await Product.findByIdAndDelete(id);
+        if( response ){
+            res.status(201).json({
+                msg: "Product Deleted Successfully"
+            });
+        }
+        res.status(400).json(response);
+        
+    }
+    catch (err) {
+        res.status(404).json(err);
     }
 }
--- routes/product.js
+++ routes/product.js
@@ -4,8 +4,17 @@ const router = Router();
 
 const { protect } = require("../middelwares/protect.js");
 const {
-    addProduct
+    addProduct,
+    fetchProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct,
 } = require('../controllers/product.js');
 
+
+router.route('/').get(protect,fetchProducts);
 router.route('/create').post(protect, addProduct);
+router.route('/:id').get(protect,getProduct);
+router.route('/:id').put(protect,updateProduct);
+router.route('/:id').delete(protect,deleteProduct);
 module.exports = router; 
